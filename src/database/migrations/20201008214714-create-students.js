3e8f0900deb2c1398ca5fa1fc0631d5c74c61c28
Gustavo Henrique Dias <31208960+GustavoDiias@users.noreply.github.com>
2020-10-08 19:36:57 -0300

    Criação da Tabela Student

---
+++ src/database/migrations/20201008214714-create-students.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('student', {
+      ra: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        unique: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      cpf: {
+        type: Sequelize.BIGINT,
+        allowNull: false,
+        unique: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
