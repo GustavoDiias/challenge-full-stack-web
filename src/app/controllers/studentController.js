1d0b5f27e6d6ed0a125045b033fee2ec6625aeed
Gustavo Henrique Dias <31208960+GustavoDiias@users.noreply.github.com>
2020-10-09 16:25:40 -0300

    Exclusão dos dados(DELETE)

--- src/app/controllers/studentController.js
+++ src/app/controllers/studentController.js
@@ -1,22 +1,10 @@
 const express = require('express');
+const { sequelize, QueryTypes } = require('sequelize');
+const { upsert } = require('../models/student');
 
 const Student = require('../models/student');
 
 const router = express.Router();
-
-router.post('/register', async (req, res) => {
-    const { ra, name, email, cpf } = req.body;
-
-    try {
-
-        const student = await Student.create({ ra, name, email, cpf });
-
-        return res.json(student);
-
-    } catch (err) {
-        return res.status(400).send({ error: 'Erro ao cadastrar novo estudante' });
-    }
-});
 
 router.get('/', async (req, res) => {
 
@@ -31,8 +19,56 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.post('/create', async (req, res) => {
+    const { ra, name, email, cpf } = req.body;
+
+    try {
+
+        const uniqueRa = await Student.findAll({
+            attributes: ['ra']
+        })
+
+        const student = await Student.create({ ra, name, email, cpf });
+
+        return res.json(student);
 
 
+    } catch (err) {
+        console.log(err);
+        return res.status(400).send({ error: 'Erro ao cadastrar novo estudante' });
+    }
+});
 
+router.put('/update/:student_id', async (req, res) => {
+    const { id } = req.body;
+
+    try {
+
+        return res.json(student);
+
+    } catch (err) {
+        return res.status(400).send({ error: 'Erro ao cadastrar novo estudante' });
+    }
+});
+
+router.delete('/delete/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+
+        const student = await Student.findByPk(id);
+
+        if (!student) {
+            return res.status(400).send({ error: 'Estudante não encontrado' });
+        }
+
+        await student.destroy();
+
+        return res.json();
+
+    } catch (err) {
+        return res.status(400).send({ error: 'Erro ao cadastrar novo estudante' });
+    }
+});
 
 module.exports = app => app.use('/student', router);
--- src/database/migrations/20201008214714-create-students.js
+++ src/database/migrations/20201008214714-create-students.js
@@ -37,11 +37,8 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+
+    await queryInterface.dropTable('students');
+
   }
 };
